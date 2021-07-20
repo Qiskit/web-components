47563d392f85cc0c6a1563bfe0e77db9c2186ea3
Abdón Rodríguez Davila <a@abdonrd.com>
2021-07-20 10:31:42 +0200

    Setup rollup-plugin-postcss to import styles from scss

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,16 +6,30 @@
  */
 
 const typescript = require('@rollup/plugin-typescript');
+const postcss = require('rollup-plugin-postcss');
+const { default: postcssLit } = require('rollup-plugin-postcss-lit');
 
 module.exports = {
   stories: ['../components/**/*.stories.ts'],
 
   rollupConfig(config) {
-    config.plugins.unshift(
+    config.plugins.push(
       typescript({
         declaration: false,
         sourceMap: false,
         inlineSources: false,
+      })
+    );
+    config.plugins.push(
+      postcss({
+        inject: false,
+        include: 'components/*/*.scss',
+      })
+    );
+    config.plugins.push(
+      postcssLit({
+        include: 'components/*/*.scss',
+        importPackage: 'lit',
       })
     );
 
---
+++ wds-base.config.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) IBM, Corp. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { fromRollup } from '@web/dev-server-rollup';
+import rollupPostcss from 'rollup-plugin-postcss';
+import rollupPostcssLit from 'rollup-plugin-postcss-lit';
+
+const postcss = fromRollup(rollupPostcss);
+const postcssLit = fromRollup(rollupPostcssLit.default);
+
+export default {
+  nodeResolve: true,
+  mimeTypes: {
+    '**/*.scss': 'js',
+  },
+  plugins: [
+    esbuildPlugin({ ts: true }),
+    postcss({
+      inject: false,
+      include: 'components/*/*.scss',
+    }),
+    postcssLit({
+      include: 'components/*/*.scss',
+      importPackage: 'lit',
+    }),
+  ],
+};
