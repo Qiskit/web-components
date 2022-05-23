99dc163b2903eaf2612ef903137a3107fdcc2a3f
Gregorio Iniesta <korgan00@gmail.com>
2022-05-23 20:06:39 +0200

    better story code

--- components/mega-menu-dropdown/index.stories.ts
+++ components/mega-menu-dropdown/index.stories.ts
@@ -14,7 +14,47 @@ export default {
   title: 'Components/MegaMenuDropdown',
 };
 
-const menuColumn1 = [
+const singleColumnGroups = [
+  {
+    title: { label: 'Integer vitae ornare ipsum', url: 'example.com' },
+    content: [
+      { label: 'Eget ullamcorper lorem', url: 'example.com' },
+      { label: 'Sed et felis scelerisque, malesuada', url: 'example.com' },
+      { label: 'Ipsum ac', url: 'example.com' },
+      { label: 'Eget ullamcorper lorem', url: 'example.com' },
+      { label: 'Sed et felis scelerisque, malesuada', url: 'example.com' },
+      { label: 'Ipsum ac', url: 'example.com' },
+    ],
+  },
+];
+
+const twoColumnsGroups = [
+  {
+    title: { label: 'Lorem ipsum dolor', url: 'example.com' },
+    content: [
+      { label: 'Sit amet', url: 'example.com' },
+      { label: 'Consectetur adipiscing elit', url: 'example.com' },
+      { label: 'Integer mattis nulla vitae nibh', url: 'example.com' },
+      { label: 'Sit amet', url: 'example.com' },
+      { label: 'Consectetur adipiscing elit', url: 'example.com' },
+      { label: 'Integer mattis nulla vitae nibh', url: 'example.com' },
+    ],
+  },
+  {
+    title: { label: 'Pharetra pharetra', url: 'example.com' },
+    content: [
+      { label: 'Pellentesque interdum semper', url: 'example.com' },
+      { label: 'Justo', url: 'example.com' },
+      { label: 'Suspendisse ut finibus elit', url: 'example.com' },
+      { label: 'Pulvinar sit amet iaculis id', url: 'example.com' },
+      { label: 'Lacinia quis tortor', url: 'example.com' },
+      { label: 'Praesent hendrerit vitae', url: 'example.com' },
+      { label: 'Metus feugiat fringilla', url: 'example.com' },
+    ],
+  },
+];
+
+const threeColumnsGroups = [
   {
     title: { label: 'Lorem ipsum dolor', url: 'example.com' },
     content: [
@@ -35,9 +75,6 @@ const menuColumn1 = [
       { label: 'Metus feugiat fringilla', url: 'example.com' },
     ],
   },
-];
-
-const menuColumn2 = [
   {
     title: { label: 'Integer vitae ornare ipsum', url: 'example.com' },
     content: [
@@ -49,9 +86,6 @@ const menuColumn2 = [
       { label: 'Ipsum ac', url: 'example.com' },
     ],
   },
-];
-
-const menuColumn3 = [
   {
     title: { label: 'Semper est', url: 'example.com' },
     content: [
@@ -73,29 +107,29 @@ const menuColumn3 = [
 const singleColumn = [
   {
     title: 'Block 1',
-    content: menuColumn1,
+    content: singleColumnGroups,
   },
 ];
 
 const tripleColumn = [
   {
     title: 'Block 1',
-    content: [...menuColumn1, ...menuColumn2, ...menuColumn3],
+    content: threeColumnsGroups,
   },
 ];
 
 const tripleColumnMultiBlock = [
   {
     title: 'Block 1',
-    content: menuColumn1,
+    content: singleColumnGroups,
   },
   {
     title: 'Block 2',
-    content: [...menuColumn1, ...menuColumn2],
+    content: twoColumnsGroups,
   },
   {
     title: 'Block 3',
-    content: [...menuColumn1, ...menuColumn2, ...menuColumn3],
+    content: threeColumnsGroups,
   },
 ];
 
