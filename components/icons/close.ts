9d79e60459017a8953eba502f534e831239e56cc
Abdón Rodríguez Davila <a@abdonrd.com>
2022-06-22 18:37:28 +0200

    Change svg to html in icons

--- components/icons/close.ts
+++ components/icons/close.ts
@@ -5,12 +5,15 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { svg } from 'lit';
+import { html } from 'lit';
 
-export const closeIcon = svg`
+export const closeIcon = html`
   <svg id="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
     <title>close</title>
-    <polygon class="cls-1" points="24 9.4 22.6 8 16 14.6 9.4 8 8 9.4 14.6 16 8 22.6 9.4 24 16 17.4 22.6 24 24 22.6 17.4 16 24 9.4" />
+    <polygon
+      class="cls-1"
+      points="24 9.4 22.6 8 16 14.6 9.4 8 8 9.4 14.6 16 8 22.6 9.4 24 16 17.4 22.6 24 24 22.6 17.4 16 24 9.4"
+    />
     <rect class="cls-2" width="32" height="32" />
     <style>
       .cls-1 {
