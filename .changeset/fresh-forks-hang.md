2f25621d178c6f1c59d88f97be23e85ec59b40a0
Goyo <korgan00@gmail.com>
2022-06-30 10:40:25 +0200

    Added a11y and a11y-test to the mega menu dropdown (#118)
    * added a11y and tests to the mega menu dropdown
    
    * Create fresh-forks-hang.md
    
    * changed mock data name appending test to prevent adding it to npm package

---
+++ .changeset/fresh-forks-hang.md
@@ -0,0 +1,5 @@
+---
+"@qiskit/web-components": patch
+---
+
+Added a11y and a11y-test to the mega menu dropdown
---
+++ components/mega-menu-dropdown/index.test.ts
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) IBM, Corp. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { fixture, expect } from '@open-wc/testing';
+import { html } from 'lit/static-html.js';
+
+import { tripleColumnMultiBlock } from './mockData.test.js';
+
+import './index.js';
+
+describe('mega menu dropdown', () => {
+  it('passes accessibility test', async () => {
+    const el = await fixture(
+      html`<qiskit-mega-menu-dropdown
+        placeholder="Browse all content"
+        .content="${tripleColumnMultiBlock}"
+      ></qiskit-mega-menu-dropdown>`
+    );
+    await expect(el).to.be.accessible();
+  });
+});
