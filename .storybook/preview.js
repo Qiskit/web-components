0ffdb2c138feb380918cbe8c364add75bbcd381c
Abdón Rodríguez Davila <a@abdonrd.com>
2022-07-26 15:44:21 +0200

    Update dependencies (#128)
    * Update dependencies
    
    * Use Lit render

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { render } from 'lit-html';
+import { render } from 'lit';
 import { addons } from '@web/storybook-prebuilt/addons.js';
 
 export const parameters = {
