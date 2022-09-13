d5088697ea015eaf45497e98d993d7fad8d69a7a
techtolentino <makertolentino@gmail.com>
2022-09-13 13:34:40 -0700

    add type inferences

--- components/ui-shell/index.stories.ts
+++ components/ui-shell/index.stories.ts
@@ -15,7 +15,7 @@ export default {
 
 export const Default = () =>
   html`<qiskit-ui-shell
-    @on-click="${(e) => {
+    @on-click="${(e: { detail: { label: string; url: string } }) => {
       alert(`label: ${e.detail?.label}, url: ${e.detail?.url}`);
     }}"
   ></qiskit-ui-shell>`;
