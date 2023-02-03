fffed9f45c04be6888db2a2c2cc09d71c9509760
Junye Huang <h.jun.ye@gmail.com>
2023-02-03 18:15:11 +0100

    update slack link

--- components/ui-shell/settings.ts
+++ components/ui-shell/settings.ts
@@ -40,7 +40,7 @@ export const SOCIAL_LINKS: SocialLinks[] = [
   {
     icon: slackIcon,
     label: 'Slack',
-    url: 'https://ibm.co/joinqiskitslack',
+    url: ' https://qisk.it/join-slack',
   },
   {
     icon: youtubeIcon,
@@ -195,7 +195,7 @@ export const NAV_ITEMS: TopLevelNavItem[] = [
           },
           {
             label: 'Slack',
-            url: 'https://ibm.co/joinqiskitslack',
+            url: ' https://qisk.it/join-slack',
           },
           {
             label: 'Qiskit Textbook',
